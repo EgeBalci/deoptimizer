ceb439ea6845a61475fa68b17ec6e3106555d188
Ege Balcı <egebalci@pm.me>
2024-01-16 15:08:50 +0100

    CE transform bug fixes.

--- src/x86_64/transforms/condition_expand.rs
+++ src/x86_64/transforms/condition_expand.rs
@@ -11,35 +11,47 @@ pub fn apply_ce_transform(
         return Err(DeoptimizerError::TransformNotPossible);
     }
     let mut asm = CodeAssembler::new(bitness)?;
+    let bt = inst.near_branch_target();
     let mut test = match bitness {
         16 => Instruction::with2(Code::Test_rm16_r16, Register::CX, Register::CX)?,
         32 => Instruction::with2(Code::Test_rm32_r32, Register::ECX, Register::ECX)?,
         64 => Instruction::with2(Code::Test_rm64_r64, Register::RCX, Register::RCX)?,
         _ => return Err(DeoptimizerError::InvalidProcessorMode),
     };
-    // test.set_ip(inst.ip());
     test = *rencode(bitness, [test].to_vec(), inst.ip())?
         .first()
         .unwrap();
-    let bt = inst.near_branch_target();
 
     if inst.is_loopcc() || inst.is_loop() {
+        let mut dec = match bitness {
+            16 => Instruction::with2(Code::Dec_r16, Register::CX, Register::CX)?,
+            32 => Instruction::with2(Code::Dec_r32, Register::ECX, Register::ECX)?,
+            64 => Instruction::with2(Code::Dec_rm64, Register::RCX, Register::RCX)?,
+            _ => return Err(DeoptimizerError::InvalidProcessorMode),
+        };
+        dec = *rencode(bitness, [dec].to_vec(), inst.ip())?
+            .first()
+            .unwrap();
         match inst.mnemonic() {
             Mnemonic::Loop | Mnemonic::Loope => {
-                asm.jz(bt)?;
-                let insts = asm.instructions();
-                let mut jz = insts.first().unwrap().clone();
-                jz.set_ip(test.next_ip());
-                jz.as_near_branch();
-                return Ok(rencode(bitness, [test, jz].to_vec(), inst.ip())?);
-            }
-            Mnemonic::Loopne => {
                 asm.jnz(bt)?;
                 let insts = asm.instructions();
                 let mut jnz = insts.first().unwrap().clone();
-                jnz.set_ip(test.next_ip());
+                jnz.set_ip(dec.next_ip());
                 jnz.as_near_branch();
-                return Ok(rencode(bitness, [test, jnz].to_vec(), inst.ip())?);
+                if inst.mnemonic() == Mnemonic::Loope {
+                    return Ok(rencode(bitness, [test, jnz].to_vec(), inst.ip())?);
+                } else {
+                    return Ok(rencode(bitness, [dec, jnz].to_vec(), inst.ip())?);
+                }
+            }
+            Mnemonic::Loopne => {
+                asm.jz(bt)?;
+                let insts = asm.instructions();
+                let mut jz = insts.first().unwrap().clone();
+                jz.set_ip(dec.next_ip());
+                jz.as_near_branch();
+                return Ok(rencode(bitness, [dec, jz].to_vec(), inst.ip())?);
             }
             _ => return Err(DeoptimizerError::TransformNotPossible),
         }
